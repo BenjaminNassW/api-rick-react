37aca515b277423503c2d9a4888d7a915658f309
BenjaminNassW <benjanass@gmail.com>
2022-11-03 00:52:42 +0000

    creando la matriz

---
+++ src/components/box.jsx
@@ -0,0 +1,10 @@
+import '/workspace/api-rick-react/src/styles/box.css'
+function Box() {
+    return (
+        <div className="bg-secondary border border-dark box">
+
+        </div>
+    );
+}
+
+export default Box;
