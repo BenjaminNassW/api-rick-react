6ba3bc77fda9e80bc2b0da11a52436bbb714fffd
BenjaminNassW <benjanass@gmail.com>
2022-11-04 14:44:04 +0000

    mitad coordenada listo

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ function App() {
  
   return (
     <div className="App">
-      <header className="App-header">
+      
+      <div className="d-flex" >
+        <h1 id='marco'>A</h1><h1 id='marco'>B</h1><h1 id='marco'>C</h1><h1 id='marco'>D</h1><h1 id='marco'>E</h1><h1 id='marco'>F</h1><h1 id='marco'>G</h1><h1 id='marco'>H</h1><h1 id='marco'>I</h1><h1 id='marco'>J</h1>
+      </div>
         <div className='d-flex flex-wrap'>
           <Grid/>
 
         </div>
-      </header>
+      
     </div>
   );
 }
